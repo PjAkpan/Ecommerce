e8a5b4122ff48d7c31d2f3bd5461e60bf97a10c1
Peace Johnson <115987850+PjAkpan@users.noreply.github.com>
2022-12-27 22:33:35 +0100

    first commit

---
+++ src/component/signup.component.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Middle name</label>
+          <input type="text" className="form-control" placeholder="Middle name" />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Phone Number</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Phone Number"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Gender</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter gender"
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    )
+  }
+}
